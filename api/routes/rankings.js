c912caae04d33009af9c8cbf7bffb9489dc125be
molivaresr <m.olivares.r@gmail.com>
2022-03-02 11:02:44 +0100

    comentarios para ranking

--- api/routes/rankings.js
+++ api/routes/rankings.js
@@ -4,9 +4,9 @@ const {getPlayers, getRanking} = require('../controllers/rankings')
 
 router.get('/', getPlayers);
 
-router.get('/ranking', getRanking);
+router.get('/ranking', getRanking); // Esta línea no funciona
 
-router.get('/ranking/lastplayer', getRanking);
+router.get('/ranking/lastplayer', getRanking); //Aqui funciona 
 
 router.get('/ranking/firstplayer', (req, res) => {
     res.status(200).send('Primera Posición')
