1a04ef9201e618340081a4447573e840064ac706
molivaresr <m.olivares.r@gmail.com>
2022-04-26 16:53:44 +0200

    Cleaning Repo

--- DEL_jsclient/src/config/giphy.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = 'tOHSVKjYabUbzkE3WkJvar2vJnS6QitU';
-export const API_URL = `https://api.giphy.com/v1/gifs/search?api_key={API_KEY}&tag=404&limit=1&offset=0&rating=g&lang=en`;
--- DEL_jsclient/src/context/socket.js
+++
@@ -1,4 +0,0 @@
-import io from "socket.io-client";
-import {SOCKET_URL} from '../config/defaultConf';
-
-export const socket = io(SOCKET_URL);
